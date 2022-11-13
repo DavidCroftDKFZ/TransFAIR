a11adfb34a124fef3aa55ebb4a1b3cda28af29ec
Julian Doerenberg <julian.doerenberg@rwth-aachen.de>
2022-11-13 13:00:17 +0100

    Added corpus for gPAS connector of idmapping

--- src/main/java/de/samply/transfair/converters/IDMapping/gPAS_Mapping.java
+++ src/main/java/de/samply/transfair/converters/IDMapping/gPAS_Mapping.java
@@ -1,5 +1,19 @@
 package de.samply.transfair.converters.IDMapping;
 
-public class gPAS_Maping extends ID_Mapping{
+import org.jetbrains.annotations.NotNull;
 
+public class gPAS_Mapping extends ID_Mapping{
+
+    //TODO: Javadoc
+    //TODO: Add reading host and port from configuration
+    @Override
+    public String fetch_mapping(@NotNull String id, @NotNull String src_domain, @NotNull String tar_domain) throws Exception {
+        String gpas_host = "localhost";
+        String gpas_port = "8085";
+        String tar_id = "";//TODO: just initialize and take from gPAs SOP response
+
+        //TODO: SOAP call
+
+        return tar_id;
+    }
 }
