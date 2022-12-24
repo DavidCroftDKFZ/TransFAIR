f543b7857a0f0372443598ceecd87eeb99c9d141
Enola Knezevic <enola.knezevic@dkfz-heidelberg.de>
2022-12-24 13:27:59 +0100

    specimen mapping from BBMRI to MII test

--- src/test/java/de/samply/transfair/resources/SpecimenMappingTest.java
+++ src/test/java/de/samply/transfair/resources/SpecimenMappingTest.java
@@ -1,11 +1,16 @@
 package de.samply.transfair.resources;
 
 import static de.samply.transfair.JsonUtils.compareFhirObjects;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.hl7.fhir.r4.model.CanonicalType;
 import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Quantity;
+import org.hl7.fhir.r4.model.Range;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Specimen;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,6 +39,15 @@ public class SpecimenMappingTest {
 
     Patient patient = new Patient();
     patient.setId("patientId");
+    
+    Calendar cal = Calendar.getInstance();
+    cal.set(Calendar.YEAR, 1997);
+    cal.set(Calendar.MONTH, Calendar.OCTOBER);
+    cal.set(Calendar.DAY_OF_MONTH, 22);
+    Date collectedDate = cal.getTime();
+    
+//    DateTimeType collected = new DateTimeType();
+//    collected.setValueAsString("1997-10-22T00:00:00+02:00");
 
     specimenBbmriForConverting = new Specimen();
     specimenBbmriForConverting.setId("specimenId");
@@ -45,7 +59,7 @@ public class SpecimenMappingTest {
     CodeableConcept codeableConceptBbmriForConvertingDiagnosis = new CodeableConcept();
     codeableConceptBbmriForConvertingDiagnosis.getCodingFirstRep().setSystem("http://hl7.org/fhir/sid/icd-10").setCode("C61");
     diagnosisExtensionBbmri.setValue(codeableConceptBbmriForConvertingDiagnosis);
-    
+
     Extension storageTemperatureExtensionBbmri = new Extension();
     storageTemperatureExtensionBbmri.setUrl("https://fhir.bbmri.de/StructureDefinition/StorageTemperature");
     CodeableConcept codeableConceptBbmriForConvertingStorageTemperature = new CodeableConcept();
@@ -53,12 +67,43 @@ public class SpecimenMappingTest {
     storageTemperatureExtensionBbmri.setValue(codeableConceptBbmriForConvertingStorageTemperature);
     specimenBbmriForConverting.setExtension(List.of(diagnosisExtensionBbmri, storageTemperatureExtensionBbmri));
 
+    specimenBbmriForConverting.setExtension(List.of(diagnosisExtensionBbmri, storageTemperatureExtensionBbmri));
+
+    CodeableConcept codeableConceptBbmriForConvertingSampleType = new CodeableConcept();
+    codeableConceptBbmriForConvertingSampleType.getCodingFirstRep().setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType").setCode("blood-plasma");
+
+    specimenBbmriForConverting.setType(codeableConceptBbmriForConvertingSampleType);
+    
+    specimenBbmriForConverting.getCollection().getCollectedDateTimeType().setValue(collectedDate);
+
+
+    specimenMiiForComparing = new Specimen();
+    specimenMiiForComparing.setId("specimenId");
+    specimenMiiForComparing.getMeta().setProfile(List.of(new CanonicalType("https://www.medizininformatik-initiative.de/fhir/ext/modul-biobank/StructureDefinition/Specimen")));
+    specimenMiiForComparing.setSubject(new Reference().setReference(patient.getId()));
+    Extension storageTemperatureExtensionMiiForComparing = new Extension();
+    storageTemperatureExtensionMiiForComparing.setUrl(
+        "https://www.medizininformatik-initiative.de/fhir/ext/modul-biobank/StructureDefinition/Temperaturbedingungen");
+    storageTemperatureExtensionMiiForComparing.setValue(new Range().setHigh(new Quantity(-195)).setLow(new Quantity(-160)));
+
+    specimenMiiForComparing.getCollection().setExtension(List.of(storageTemperatureExtensionMiiForComparing));
+    
+    CodeableConcept coding = new CodeableConcept();
+    coding.getCodingFirstRep().setCode("119361006").setSystem("http://snomed.info/sct");
+    specimenMiiForComparing.setType(coding);
+
+    specimenMiiForComparing.getCollection().getCollectedDateTimeType().setValue(collectedDate);
+
+
+
+
+
 
     specimenMiiForConverting = new Specimen();
     specimenMiiForConverting.setId("specimenId");
     specimenMiiForConverting.getMeta().setProfile(List.of(new CanonicalType("https://www.medizininformatik-initiative.de/fhir/ext/modul-biobank/StructureDefinition/Specimen")));
     specimenMiiForConverting.setSubject(new Reference().setReference(patient.getId()));
-    
+
     CodeableConcept bodySiteCode = new CodeableConcept();
     bodySiteCode.getCodingFirstRep().setCode("8148/2");
     bodySiteCode.getCodingFirstRep().setSystem("urn:oid:1.3.6.1.4.1.19376.1.3.11.36");
@@ -74,7 +119,7 @@ public class SpecimenMappingTest {
     specimenMapping.fromBbmri(specimenBbmriForConverting);
     specimen2Mii = specimenMapping.toMii();
 
-    compareFhirObjects(specimen2Mii, specimenMiiForConverting);
+    compareFhirObjects(specimen2Mii, specimenMiiForComparing);
   }
 
   @Test
